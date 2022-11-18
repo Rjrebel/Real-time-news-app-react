7d6fe42d73d45ea3ac23a678b6cee1a8129b478b
Rohit Jibhakate <rohitjibhakate111@gmail.com>
2022-11-18 15:21:25 +0530

    NewsItem modified and News Modified, Sample json file for news created

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-        <>
-      <div>This is news Component</div>
-      <NewsItem />
+      <>
+        <div className="container my-3">
+          <h1 className="p-3">News 24x7 - Top Headlines</h1>
+          <div className="row g-3">
+            <div className="col-md-4"><NewsItem  title="myTtitle" description="this is desc" /></div>
+            <div className="col-md-4"><NewsItem  title="myTtitle" description="this is desc" /></div>
+            <div className="col-md-4"><NewsItem  title="myTtitle" description="this is desc" /></div>
+         
+          </div>
+        </div>
       </>
-    )
+    );
   }
 }
 
-export default News
+export default News;
