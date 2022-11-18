7af1a374f784d7174e41aa6cdea57785ca19f4e5
Rohit Jibhakate <rohitjibhakate111@gmail.com>
2022-11-18 16:38:04 +0530

    Articles fetched using fetch api from news api and displayed in news item

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
     let {title , description, imageUrl, newsUrl} = this.props;
     return (
       <div className="card" style={{ width: "18rem" }}>
-        <img src={imageUrl} className="card-img-top" alt="..." />
+        <img src={imageUrl ? imageUrl : "https://aylien.com/images/uploads/logos/News_API_logo_green_black_large.svg"} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">
